689103b429e59a16c2174ca034279ced785bd832
Jhonathan <jhonathan_galhardo@hotmail.com>
2025-08-09 01:09:21 -0300

    chore: add supabase fake keys

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseUrl = '123';
+const supabaseKey = '123';
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error('Supabase URL and Key are required');
