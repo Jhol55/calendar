f4b493bb8bcf0bdd3e158db049657b79bbcc14d7
Jhonathan <jhonathan_galhardo@hotmail.com>
2025-08-11 16:01:45 -0300

    feat(register): update register api route

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,13 +1,15 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { sessionService } from '@/services/session';
 import { userService } from '@/services/user';
+import { prisma } from '@/services/prisma';
 
 export async function POST(request: NextRequest) {
   const requestData = await request.json();
+
   const { repeatPassword, ...filteredData } = requestData;
 
   if (repeatPassword !== requestData.password) {
-    return NextResponse.json({ success: false });
+    return NextResponse.json({ success: false, status: 400 });
   }
 
   const encryptedData = {
@@ -15,19 +17,13 @@ export async function POST(request: NextRequest) {
     password: userService.hashPassword(requestData.password),
   };
 
-  if (encryptedData) {
-    console.log(userService.generateValidationCodeFromEmail(requestData.email));
-  }
-
-  const error = true;
-
-  if (error) {
-    return NextResponse.json({ success: false });
-  }
+  const newUser = await prisma.user.create({
+    data: encryptedData,
+  });
 
   await sessionService.deleteSession();
   await sessionService.createSession(requestData);
   console.log(userService.generateValidationCodeFromEmail(requestData.email));
 
-  return NextResponse.json({ success: true });
+  return NextResponse.json({ success: true, status: 201, data: newUser });
 }
