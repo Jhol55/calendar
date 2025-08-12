6ff82c97f67a7800a041f0a4e3ce1aab2f32a7f8
Jhonathan <jhonathan_galhardo@hotmail.com>
2025-08-12 15:56:14 -0300

    refactor(api): change base url for production

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const baseURL =
+  process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:3000/api';
+
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL,
   headers: {
     'Content-Type': 'application/json',
   },
