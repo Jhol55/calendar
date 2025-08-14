8ff8a5b88871d3eb4bf1043797d503ee3d32f093
Jhonathan <jhonathan_galhardo@hotmail.com>
2025-08-14 11:29:33 -0300

    feat(webpack): add production webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,27 @@
+import webpackObfuscator from 'webpack-obfuscator';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone"
+  output: "standalone",
+  webpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.module.rules.push({
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        enforce: 'post',
+        use: [
+          {
+            loader: webpackObfuscator.loader,
+            options: {
+              compact: true,
+            }
+          }
+        ]
+      });
+    }
+
+    return config;
+  }
 };
 
 export default nextConfig;
