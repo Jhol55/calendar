cb6b830e13133bfd0b89adb72ce3c449f0ccd921
Jhonathan <jhonathan_galhardo@hotmail.com>
2025-08-14 19:42:49 -0300

    feat(xior): change axios to xior

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,13 @@
 import xior from 'xior';
 
+const internalBaseURL = 'http://app:3000/api';
+const externalBaseURL = process.env.NEXT_PUBLIC_API_URL;
+
 const baseURL =
   process.env.NODE_ENV === 'production'
-    ? 'http://app:3000/api'
+    ? typeof window === 'undefined'
+      ? internalBaseURL
+      : externalBaseURL
     : 'http://localhost:3000/api';
 
 export const api = xior.create({
