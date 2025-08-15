22e80221243fd96fd6f9a0c8369d4b75cba0dde1
Jhonathan <jhonathan_galhardo@hotmail.com>
2025-08-15 19:42:31 -0300

    feat(layout): add sidebar

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,6 +3,8 @@
 import { useEffect } from 'react';
 import { useUser } from '@/hooks/use-user';
 import { userService } from '@/services/user';
+import { useState } from 'react';
+import { Sidebar, SidebarToggle } from '@/components/layout/sidebar';
 
 export default function RootLayout({
   children,
@@ -12,12 +14,29 @@ export default function RootLayout({
   const { setUser } = useUser();
 
   useEffect(() => {
-    const getUser = async () => {
+    const fetchUser = async () => {
       const response = await userService.getUser();
+      console.log(response);
       setUser(response);
     };
-    getUser();
+    fetchUser();
   }, [setUser]);
 
-  return <>{children}</>;
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  return (
+    <div className="flex flex-col h-screen bg-zinc-900">
+      <Sidebar
+        isOpen={isSidebarOpen}
+        onToggle={() => setIsSidebarOpen(!isSidebarOpen)}
+      />
+
+      <SidebarToggle
+        isOpen={isSidebarOpen}
+        onToggle={() => setIsSidebarOpen(!isSidebarOpen)}
+      />
+
+      <main className="flex-1 overflow-auto">{children}</main>
+    </div>
+  );
 }
